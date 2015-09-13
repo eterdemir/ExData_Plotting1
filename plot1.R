1ae73685394ed9ce3562e9e897ddfb9ec9c1206a
eterdemir <eliftokar@gmail.com>
2015-09-13 00:26:34 -0500

    plot1 R code
    Plot 1 R Code

---
+++ plot1.R
@@ -0,0 +1,13 @@
+setwd("C:/Users/Erdemir/Desktop/Coursera - Data Science/Course4-ExploratoryDataAnalysis/Course Project 1/exdata-data-household_power_consumption")
+
+mydata <- read.table("household_power_consumption.txt", header=TRUE, sep=";")
+
+mydata$Date <- as.Date(mydata$Date, format = "%d/%m/%Y")
+
+mydata.sub <- subset(mydata, Date == "2007-02-01" | Date == "2007-02-02")
+
+x <- as.numeric(levels(mydata.sub$Global_active_power)[mydata.sub$Global_active_power])
+
+png(file = "plot1.png")
+hist(x, col = "red", xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
+dev.off()
